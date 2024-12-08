55ced10ff8c5a4057b74dd13d7d9fbd82e130a9b
Daniel <dereniowskid885@gmail.com>
2024-12-08 20:22:23 +0100

    code improvements, loading spinner added to button component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    // hides nextjs-toast that shows static/dynamic route message
+    appIsrStatus: false
+  }
 };
 
 export default nextConfig;
