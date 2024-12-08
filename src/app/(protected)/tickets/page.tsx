b76d8af6e423c6812ab7d35f93519975e6ec7bfc
Daniel <dereniowskid885@gmail.com>
2024-12-08 20:34:34 +0100

    NavLink component, NavLayout, pages placeholders added

---
+++ src/app/(protected)/tickets/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Tickets() {
+  return <h1>Tickets</h1>;
+}
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,8 +1,11 @@
-export const DASHBOARD_PATH = '/';
+export const HOME_PATH = '/';
 export const LOGIN_PATH = '/login';
 export const REGISTER_PATH = '/register';
 export const THEME_PATH = '/theme';
 export const TYPOGRAPHY_PATH = '/typography';
+export const TICKETS_PATH = '/tickets';
+export const FAVORITES_PATH = '/favorites';
+export const PROFILE_PATH = '/profile';
 
-export const protectedRoutes = [DASHBOARD_PATH];
+export const protectedRoutes = [HOME_PATH, TICKETS_PATH, FAVORITES_PATH, PROFILE_PATH];
 export const publicRoutes = [LOGIN_PATH, REGISTER_PATH, THEME_PATH, TYPOGRAPHY_PATH];
