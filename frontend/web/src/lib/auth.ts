09a0bfac53aa4338de48d12b5f5b96e19e2256e3
Daniel <dereniowskid885@gmail.com>
2024-12-20 17:21:38 +0100

    registerHandler todo removal

--- frontend/web/src/lib/auth.ts
+++ frontend/web/src/lib/auth.ts
@@ -52,22 +52,9 @@ export const loginHandler = async (formData: ILoginForm): Promise<IResult> => {
   }
 };
 
-// TODO: extend backend register endpoint with missing values
-export const registerHandler = async ({
-  email,
-  name,
-  surname,
-  password,
-  city
-}: IRegisterForm): Promise<IResult> => {
+export const registerHandler = async (formData: IRegisterForm): Promise<IResult> => {
   try {
-    const response = await axios.post(API_REGISTER_URL, {
-      email,
-      password,
-      name,
-      surname,
-      city
-    });
+    const response = await axios.post(API_REGISTER_URL, formData);
 
     if (response.status === 201) {
       return { success: true };
