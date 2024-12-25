d907490d941bde9526558a51ce1e51fd255ece36
Daniel Dereniowski <leinad885@o2.pl>
2024-12-25 10:55:50 +0100

    Account verification page changed to server component

--- frontend/web/src/app/(auth)/verify/[token]/page.tsx
+++ frontend/web/src/app/(auth)/verify/[token]/page.tsx
@@ -1,54 +1,36 @@
-'use client';
-
-import { accountVerifyHandler } from '@/lib/auth';
-import { getLastPathSegment } from '@/lib/helpers';
-import { getTokenPayload, isTokenExpired } from '@/lib/token';
-import { usePathname } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import { isTokenExpired } from '@/lib/token';
+import React from 'react';
 import { Card, CardFooter, CardHeader, CardTitle } from '@/components/shadcn/card';
 import { Typography } from '@/components/ui/Typography';
 import Link from 'next/link';
 import { Button } from '@/components/shadcn/button';
 import { LOGIN_PATH } from '@/constants/routes';
+import { accountVerifyHandler } from '@/lib/auth';
 
-export default function AccountVerifyPage() {
-  const pathname = usePathname();
-  const token = getLastPathSegment(pathname);
+export interface IAccountVerifyPage {
+  params: { token: string };
+}
+
+export default async function AccountVerifyPage({ params }: IAccountVerifyPage) {
+  let isAccountVerifySuccess = false;
+
+  const token = params.token;
   const tokenExpired = isTokenExpired(token);
-  const tokenPayload = getTokenPayload(token);
 
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const [isAccountVerifySuccess, setAccountVerifySuccess] = useState<boolean>(!tokenExpired);
-
-  useEffect(() => {
-    const verificationHandler = async () => {
-      const result = await accountVerifyHandler({ token });
-
-      if (result.success) {
-        setAccountVerifySuccess(true);
-      } else {
-        setErrorMessage(result.error ?? '');
-      }
-    };
-
-    verificationHandler();
-  }, [token, tokenPayload.email]);
+  if (!tokenExpired) {
+    const result = await accountVerifyHandler({ token });
+    isAccountVerifySuccess = result.success;
+  }
 
   return (
     <Card className="w-screen rounded-none border-none bg-primary-800 sm:w-auto sm:min-w-[400px]">
       <CardHeader className="space-y-4">
         <CardTitle>
-          {errorMessage ? (
-            <Typography variant="h3" className="text-center font-normal text-error">
-              {errorMessage}
-            </Typography>
-          ) : (
-            <Typography variant="h3" className="text-center font-normal text-white">
-              {isAccountVerifySuccess
-                ? 'Konto zostało aktywowane!'
-                : 'Link do aktywacji konta wygasł.'}
-            </Typography>
-          )}
+          <Typography variant="h3" className="text-center font-normal text-white">
+            {isAccountVerifySuccess
+              ? 'Konto zostało aktywowane!'
+              : 'Link do aktywacji konta wygasł'}
+          </Typography>
         </CardTitle>
       </CardHeader>
       <CardFooter className="flex justify-center">
