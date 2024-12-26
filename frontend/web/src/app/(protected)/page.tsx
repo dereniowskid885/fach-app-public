d61cf536f9114ecac9a3abe59906fe3ef8785335
Daniel Dereniowski <leinad885@o2.pl>
2024-12-26 16:59:54 +0100

    NavLayout changed to MainLayout, Header and AmountIcon component added, notifications mock data added

--- frontend/web/src/app/(protected)/page.tsx
+++ frontend/web/src/app/(protected)/page.tsx
@@ -1,22 +1,3 @@
-import { Button } from '@/components/shadcn/button';
-import { Typography } from '@/components/ui/Typography';
-import { THEME_PATH, TYPOGRAPHY_PATH } from '@/constants/routes';
-import Link from 'next/link';
-
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center p-4">
-      <Typography variant="h2" className="text-white">
-        Dashboard
-      </Typography>
-      <div className="flex gap-4">
-        <Link href={THEME_PATH}>
-          <Button>Theme showcase</Button>
-        </Link>
-        <Link href={TYPOGRAPHY_PATH}>
-          <Button>Typography showcase</Button>
-        </Link>
-      </div>
-    </div>
-  );
+  return <div className="flex flex-col items-center justify-center p-4"></div>;
 }
