67e6c31dfc343d18d70df46d7509495be46356dd
Daniel <dereniowskid885@gmail.com>
2025-01-12 18:25:32 +0100

    HeaderNotificationList created

---
+++ frontend/web/src/components/ui/HeaderNotificationList.tsx
@@ -0,0 +1,31 @@
+import { notifications } from '@/mocks/notifications';
+import { Typography } from './Typography';
+import NotificationIcon from '../utilities/NotificationIcon';
+
+export default function HeaderNotificationList() {
+  return (
+    <ul>
+      <div className="divide-y divide-neutral-400">
+        {notifications.map(notification => (
+          <div
+            key={notification.id}
+            className="flex items-start space-x-3 p-4 transition-colors duration-200 hover:bg-neutral-700"
+          >
+            <NotificationIcon type={notification.type} />
+            <div className="flex min-w-0 flex-col gap-1">
+              <Typography variant="small" className="text-primary-300">
+                Fachowiec {notification.user}
+              </Typography>
+              <Typography variant="small" className="text-gray-400">
+                {notification.message} {notification.ticketId}
+              </Typography>
+              <Typography variant="note" className="text-gray-500">
+                {notification.time}
+              </Typography>
+            </div>
+          </div>
+        ))}
+      </div>
+    </ul>
+  );
+}
