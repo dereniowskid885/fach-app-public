75ac3271d156c2cb70b69949d83dd4ee2cac4c25
Daniel <dereniowskid885@gmail.com>
2025-01-12 18:25:16 +0100

    utility components added

---
+++ frontend/web/src/components/utilities/NotificationIcon.tsx
@@ -0,0 +1,19 @@
+import { BiBell, BiMessageDetail, BiWrench, BiUser } from 'react-icons/bi';
+import { ENotificationType } from '@/constants/notification';
+
+export interface INotificationIcon {
+  type: ENotificationType;
+}
+
+export default function NotificationIcon({ type }: INotificationIcon) {
+  switch (type) {
+    case ENotificationType.RESPONSE:
+      return <BiMessageDetail size={25} className="text-primary-500" />;
+    case ENotificationType.STATUS:
+      return <BiWrench size={25} className="text-secondary-500" />;
+    case ENotificationType.NEW:
+      return <BiUser size={25} className="text-accent-500" />;
+    default:
+      return <BiBell size={25} className="text-neutral-500" />;
+  }
+}
