dae7aeedf694e7ceaf8c2f52d4220d24bcf1e1ae
Daniel <dereniowskid885@gmail.com>
2025-03-08 19:03:11 +0100

    code improvements, console warnings fixes (LoadingOverlay), getUserData helper added, TicketInfoRow component created

--- frontend/web/src/app/(protected)/layout.tsx
+++ frontend/web/src/app/(protected)/layout.tsx
@@ -1,8 +1,7 @@
+import AuthWrapper from '@/components/hoc/AuthWrapper';
 import Header from '@/components/ui/Header';
 import Navigation from '@/components/ui/Navigation';
-import { getTokenPayload } from '@/lib/token';
-import { cookies } from 'next/headers';
-import { notFound } from 'next/navigation';
+import { getUserData } from '@/lib/getUserData';
 import { ReactNode } from 'react';
 
 export interface IMainLayout {
@@ -10,18 +9,15 @@ export interface IMainLayout {
 }
 
 export default async function MainLayout({ children }: IMainLayout) {
-  const token = (await cookies()).get('accessToken');
-  const tokenPayload = getTokenPayload(token?.value ?? '');
-
-  if (!tokenPayload) {
-    notFound();
-  }
+  const userData = await getUserData();
 
   return (
-    <main className="h-full w-full overflow-hidden">
-      <Header userName={tokenPayload.name} />
-      {children}
-      <Navigation />
-    </main>
+    <AuthWrapper userData={userData}>
+      <main className="h-full w-full overflow-hidden">
+        <Header userName={userData?.name ?? ''} />
+        {children}
+        <Navigation />
+      </main>
+    </AuthWrapper>
   );
 }
