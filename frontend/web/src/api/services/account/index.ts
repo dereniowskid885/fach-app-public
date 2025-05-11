c27cc01ec553d682064650e6504a31f74234d764
Daniel <dereniowskid885@gmail.com>
2025-05-11 13:21:58 +0200

    baseQueryWithReauth method added to createApi (re-calls original request after token expiration and refresh)

--- frontend/web/src/api/services/account/index.ts
+++ frontend/web/src/api/services/account/index.ts
@@ -1,12 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { isCookie } from '@/lib/isCookie';
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError
+} from '@reduxjs/toolkit/query/react';
 
-const authBaseQuery = fetchBaseQuery({
+const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_ACCOUNT_API,
   credentials: 'include'
 });
 
+// custom method which retries the request once after a 401 error, if a new access token is detected (e.g., after silent refresh).
+const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    const accessToken = await isCookie('accessToken');
+
+    // if accessToken exist after 401 error has occured, call initial request again
+    if (accessToken) {
+      result = await baseQuery(args, api, extraOptions);
+    }
+  }
+
+  return result;
+};
+
 export const api = createApi({
   reducerPath: 'accountApi',
-  baseQuery: authBaseQuery,
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({})
 });
