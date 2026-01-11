1881f71b187572c726bbc224a9ea01cfcfb1a9d9
Daniel <dereniowskid885@gmail.com>
2026-01-11 15:12:04 +0100

    Zod install in shared package, constants, helpers and middlewares file changes including error handling

--- shared/middlewares/authMiddleware.ts
+++ shared/middlewares/authMiddleware.ts
@@ -1,20 +1,25 @@
 import { Request } from "express";
 import { JwtPayload, verify } from "jsonwebtoken";
-import { AppError } from "../helpers/AppError";
+import { AppError } from "../utils/AppError";
+import { EResponseStatus } from "../constants/responseStatus";
 
 // checks if request has valid cookie with access token and saves it on req.user for further use in endpoints
 export const checkAndParseAccessToken = async (req: Request, ACCESS_TOKEN_SECRET: string) => {
   const accessToken = req.cookies.accessToken;
 
   if (!accessToken) {
-    throw new AppError("Unauthorized: No access token provided", 401);
+    throw new AppError(
+      401,
+      EResponseStatus.ERROR_TOKEN_NOT_FOUND,
+      "Unauthorized: No access token provided"
+    );
   }
 
   try {
     const payload = verify(accessToken, ACCESS_TOKEN_SECRET);
     req.user = payload as JwtPayload;
   } catch (err) {
-    throw new AppError("Forbidden: Invalid access token", 403);
+    throw new AppError(403, EResponseStatus.ERROR_INVALID_TOKEN, "Forbidden: Invalid access token");
   }
 };
 
@@ -22,18 +27,30 @@ export const checkRefreshToken = async (req: Request, REFRESH_TOKEN_SECRET: stri
   const refreshToken = req.cookies.refreshToken;
 
   if (!refreshToken) {
-    throw new AppError("Unauthorized: No refresh token provided", 401);
+    throw new AppError(
+      401,
+      EResponseStatus.ERROR_TOKEN_NOT_FOUND,
+      "Unauthorized: No refresh token provided"
+    );
   }
 
   try {
     verify(refreshToken, REFRESH_TOKEN_SECRET);
   } catch (err) {
-    throw new AppError("Forbidden: Invalid refresh token", 403);
+    throw new AppError(
+      403,
+      EResponseStatus.ERROR_INVALID_TOKEN,
+      "Forbidden: Invalid refresh token"
+    );
   }
 };
 
 export const checkUserRole = async (req: Request, roles: string[]) => {
   if (!roles.includes(req.user.role)) {
-    throw new AppError("Forbidden: Required role is missing", 403);
+    throw new AppError(
+      403,
+      EResponseStatus.ERROR_USER_INVALID_ROLE,
+      "Forbidden: Required role that allows this action is missing"
+    );
   }
 };
