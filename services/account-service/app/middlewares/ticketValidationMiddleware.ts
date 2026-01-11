ca228a2002d940a90e20985c9448586bff07a607
Daniel <dereniowskid885@gmail.com>
2026-01-11 15:06:48 +0100

    Zod install in account service, validation middleware methods added

---
+++ services/account-service/app/middlewares/ticketValidationMiddleware.ts
@@ -0,0 +1,81 @@
+import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
+import { handleZodError } from '@shared/helpers/handleZodError';
+import { ESupportedCurrency, ETicketStatus } from '@shared/constants/enums';
+import { cities } from '@shared/constants/mocks';
+
+const createTicketSchema = z
+  .object({
+    title: z.string().min(7).max(32),
+    description: z.string().min(7).max(3000),
+    categoryId: z.string().length(24),
+  })
+  .strict();
+
+export const validateCreateTicketMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    req.body = createTicketSchema.parse(req.body);
+
+    next();
+  } catch (err) {
+    handleZodError(res, err);
+  }
+};
+
+const ticketEvaluationSchema = z
+  .object({
+    price: z.object({
+      value: z.number().min(0),
+      currency: z.enum(ESupportedCurrency),
+    }),
+    minutes: z.number().int().positive(),
+  })
+  .strict();
+
+export const validateTicketEvaluationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    req.body = ticketEvaluationSchema.parse(req.body);
+
+    next();
+  } catch (err) {
+    handleZodError(res, err);
+  }
+};
+
+const ticketEvaluationAcceptSchema = z
+  .object({
+    evaluationId: z.string().length(24),
+  })
+  .strict();
+
+export const validateTicketEvaluationAcceptMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    req.body = ticketEvaluationAcceptSchema.parse(req.body);
+
+    next();
+  } catch (err) {
+    handleZodError(res, err);
+  }
+};
+
+// TODO: remove cities mock when city API is ready
+const updateTicketSchema = z
+  .object({
+    categoryId: z.string().length(24).optional(),
+    city: z.enum(cities).optional(),
+    status: z.enum(ETicketStatus).optional(),
+    assigneeId: z.string().length(24).optional(),
+    title: z.string().min(7).optional(),
+    description: z.string().min(7).optional(),
+  })
+  .strict();
+
+export const validateTicketUpdateMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    req.body = updateTicketSchema.parse(req.body);
+
+    next();
+  } catch (err) {
+    handleZodError(res, err);
+  }
+};
