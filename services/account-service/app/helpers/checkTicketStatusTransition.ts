a5b40efdf0ed7ec881c7d51270c00dbb115ad0dc
Daniel <dereniowskid885@gmail.com>
2026-01-11 16:27:26 +0100

    Account service - filterBuilder util added, interfaces and helpers added

---
+++ services/account-service/app/helpers/checkTicketStatusTransition.ts
@@ -0,0 +1,20 @@
+import { ETicketStatus } from '@shared/constants/enums';
+
+// TODO: https://github.com/dereniowskid885/fach-app/issues/10
+export const checkTicketStatusTransition = (fromStatus: string, toStatus: string) => {
+  const allowedTransitions: Record<string, string[]> = {
+    [ETicketStatus.PRICE_EVALUATION]: [ETicketStatus.PRICE_USER_ACCEPTATION, ETicketStatus.PENDING_PAYMENT],
+    [ETicketStatus.PRICE_USER_ACCEPTATION]: [ETicketStatus.PENDING_PAYMENT],
+    [ETicketStatus.PENDING_PAYMENT]: [ETicketStatus.IN_PROGRESS],
+    [ETicketStatus.IN_PROGRESS]: [ETicketStatus.SOLUTION_USER_APPROVAL, ETicketStatus.MODERATOR_INVESTIGATION],
+    [ETicketStatus.SOLUTION_USER_APPROVAL]: [ETicketStatus.COMPLETED],
+    [ETicketStatus.MODERATOR_INVESTIGATION]: [
+      ETicketStatus.IN_PROGRESS,
+      ETicketStatus.COMPLETED,
+      ETicketStatus.PRICE_EVALUATION,
+    ],
+  };
+
+  const allowedNextStatuses = allowedTransitions[fromStatus] || [];
+  return allowedNextStatuses.includes(toStatus);
+};
