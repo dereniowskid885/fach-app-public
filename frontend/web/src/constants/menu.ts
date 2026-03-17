b2290ef3a428ff01cc56bde940919359a2edeba7
Daniel <dereniowskid885@gmail.com>
2026-03-17 15:34:27 +0100

    FE - new menu structure + routes update, helpers and utils adjustments

--- frontend/web/src/constants/menu.ts
+++ frontend/web/src/constants/menu.ts
@@ -1,9 +1,90 @@
-import { ClipboardList, LayoutDashboard, MessageSquare, Settings } from 'lucide-react';
-import { HOME_PATH, ROUTES, TICKETS_PATH } from './routes';
+import {
+  ClipboardList,
+  LayoutDashboard,
+  MessageSquare,
+  Settings,
+  BriefcaseBusiness,
+  ChartColumn,
+  ClipboardCheck
+} from 'lucide-react';
+import { AVAILABLE_TICKETS_PATH, HOME_PATH, ROUTES, TICKETS_PATH } from './routes';
+import { EUserRole } from '@shared/constants/enums';
 
-export const menuItems = [
-  { id: 'dashboard', icon: LayoutDashboard, href: HOME_PATH, path: ROUTES[HOME_PATH] },
-  { id: 'tickets', icon: ClipboardList, href: TICKETS_PATH, path: ROUTES[TICKETS_PATH] },
-  { id: 'inbox', icon: MessageSquare, href: '#' },
-  { id: 'settings', icon: Settings, href: '#' }
+const commonMenuItems = [
+  {
+    id: 'dashboard',
+    translationKey: 'pages.dashboard',
+    icon: LayoutDashboard,
+    href: HOME_PATH,
+    path: ROUTES[HOME_PATH]
+  },
+  {
+    id: 'tickets',
+    translationKey: 'pages.tickets',
+    icon: ClipboardList,
+    href: TICKETS_PATH,
+    path: ROUTES[TICKETS_PATH]
+  },
+  {
+    id: 'completed-tickets',
+    translationKey: 'pages.completedTickets',
+    icon: ClipboardCheck,
+    href: '#',
+    path: {
+      en: '',
+      pl: ''
+    }
+  }
 ];
+
+const otherMenuItems = [
+  {
+    id: 'inbox',
+    translationKey: 'pages.inbox',
+    icon: MessageSquare,
+    href: '#',
+    path: {
+      en: '',
+      pl: ''
+    }
+  },
+  {
+    id: 'settings',
+    translationKey: 'pages.settings',
+    icon: Settings,
+    href: '#',
+    path: {
+      en: '',
+      pl: ''
+    }
+  }
+];
+
+const baseMenuItems = [...commonMenuItems, ...otherMenuItems];
+const specialistMenuItems = [
+  ...commonMenuItems,
+  {
+    id: 'available-tickets',
+    translationKey: 'pages.availableTickets',
+    icon: BriefcaseBusiness,
+    href: AVAILABLE_TICKETS_PATH,
+    path: ROUTES[AVAILABLE_TICKETS_PATH]
+  },
+  {
+    id: 'evaluations',
+    translationKey: 'pages.evaluations',
+    icon: ChartColumn,
+    href: '#',
+    path: {
+      en: '',
+      pl: ''
+    }
+  },
+  ...otherMenuItems
+];
+
+export const menuItemsObj = {
+  [EUserRole.USER]: baseMenuItems,
+  [EUserRole.ADMIN]: baseMenuItems,
+  [EUserRole.SPECIALIST]: specialistMenuItems
+};
--- frontend/web/src/helpers/getSpecialistPendingTicketStatusesParam.ts
+++
@@ -1,4 +0,0 @@
-import { ETicketStatus } from '@shared/constants/enums';
-
-export const getSpecialistPendingTicketStatusesParam = () =>
-  `${ETicketStatus.PRICE_EVALUATION},${ETicketStatus.PRICE_USER_ACCEPTATION}`;
