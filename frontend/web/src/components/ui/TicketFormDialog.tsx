7940e6134762f505ea09749c6cd3f4434467397c
Daniel <dereniowskid885@gmail.com>
2026-03-18 13:25:30 +0100

    TicketCreateDialog renamed to TicketFormDialog with new edit ticket functionality

--- frontend/web/src/components/ui/TicketFormDialog.tsx
+++ frontend/web/src/components/ui/TicketFormDialog.tsx
@@ -6,26 +6,40 @@ import { Textarea } from '../shadcn/textarea';
 import CategorySelect from './CategorySelect';
 import { useEffect, useState } from 'react';
 import Typography from '../common/Typography';
-import { accountApi, Category, PostTicketsApiArg, usePostTicketsMutation } from '@/api/accountApi';
+import {
+  accountApi,
+  Category,
+  Ticket,
+  usePatchTicketsByIdMutation,
+  usePostTicketsMutation
+} from '@/api/accountApi';
 import { toast } from 'sonner';
 import { useTranslations } from 'next-intl';
 import { useSelector } from 'react-redux';
 import { selectUserData } from '@/redux/slices/UserDataSlice';
 import { useErrorHandler } from '@/hooks/useErrorHandler';
+import { EActionType } from '@/constants/enums';
 
-interface ITicketCreateForm {
-  category: Category;
+interface ITicketForm {
   title: string;
   description: string;
 }
-export interface ITicketCreateDialog {
+export interface ITicketFormDialog {
   open: boolean;
+  mode: EActionType;
+  currentTicketData?: Ticket;
   closeDialog: () => void;
 }
 
-export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateDialog) {
+export default function TicketFormDialog({
+  open,
+  mode,
+  currentTicketData,
+  closeDialog
+}: ITicketFormDialog) {
   const t = useTranslations();
   const { city } = useSelector(selectUserData);
+
   const {
     register,
     handleSubmit,
@@ -33,24 +47,34 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
     watch,
     formState,
     clearErrors
-  } = useForm<ITicketCreateForm>();
+  } = useForm<ITicketForm>({
+    defaultValues: {
+      title: currentTicketData?.title,
+      description: currentTicketData?.description
+    }
+  });
 
   const [errorMessage, setErrorMessage] = useState<string | undefined>('');
   const formStateErrors = Object.values(formState.errors);
   const formError = formStateErrors.find(error => error.message)?.message;
 
-  const [ticketCategory, setTicketCategory] = useState<Category | null>(null);
+  const [ticketCategory, setTicketCategory] = useState<Category | undefined>(
+    currentTicketData?.category
+  );
   const descriptionMaxLength = 3000;
   const [descriptionCharsLeft, setDescriptionCharsLeft] = useState<number>(descriptionMaxLength);
   const descriptionInput = watch('description');
 
-  const [triggerCreateTicketMutation, { isLoading, error: errorTicketCreate }] =
+  const [triggerCreate, { isLoading: isLoadingCreate, error: errorTicketCreate }] =
     usePostTicketsMutation();
+  const [triggerEdit, { isLoading: isLoadingEdit, error: errorTicketEdit }] =
+    usePatchTicketsByIdMutation();
+  const isLoading = isLoadingCreate || isLoadingEdit;
 
   const [triggerTicketsRefetch, { error: errorTicketsRefetch }] =
     accountApi.endpoints.getTicketsMy.useLazyQuery({});
 
-  useErrorHandler(errorTicketCreate || errorTicketsRefetch, {
+  useErrorHandler(errorTicketCreate || errorTicketEdit || errorTicketsRefetch, {
     setInlineError: message => setErrorMessage(message)
   });
 
@@ -72,34 +96,54 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [open]);
 
-  const submitHandler = async (formData: ITicketCreateForm) => {
+  const submitHandler = async (formData: ITicketForm) => {
     if (!ticketCategory) {
-      setErrorMessage(t('ticketCreateDialog.errorMissingCategory'));
+      setErrorMessage(t('ticketFormDialog.errorMissingCategory'));
 
       return;
     }
 
     if (!ticketCategory?._id) {
-      setErrorMessage(t('ticketCreateDialog.errorInvalidCategory'));
+      setErrorMessage(t('ticketFormDialog.errorInvalidCategory'));
 
       return;
     }
 
-    const payload: PostTicketsApiArg = {
-      body: {
-        ...formData,
-        categoryId: ticketCategory._id,
-        city
-      }
-    };
+    let result;
 
-    const { error } = await triggerCreateTicketMutation(payload);
+    switch (mode) {
+      case EActionType.CREATION:
+        result = await triggerCreate({
+          body: {
+            ...formData,
+            categoryId: ticketCategory._id,
+            city
+          }
+        });
+
+        break;
+
+      case EActionType.EDIT:
+        if (!currentTicketData?._id) return;
+
+        result = await triggerEdit({
+          id: currentTicketData._id,
+          body: {
+            ...formData,
+            categoryId: ticketCategory._id
+          }
+        });
+
+        break;
+    }
+
+    const { error } = result;
     if (error) return;
 
     closeDialog();
     resetForm();
     triggerTicketsRefetch({});
-    toast.success(t('ticketCreateDialog.toastTitle'));
+    toast.success(t(`ticketFormDialog.toastTitle.${mode.toLowerCase()}`));
   };
 
   const ticketCreateForm = (
@@ -112,14 +156,14 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
           />
         </div>
         <div className="flex flex-col space-y-1.5">
-          <Label htmlFor="title">{t('ticketCreateDialog.titleFormLabel')}</Label>
+          <Label htmlFor="title">{t('ticketFormDialog.titleFormLabel')}</Label>
 
           <Input
             {...register('title', {
-              required: t('ticketCreateDialog.errorTitleRequired'),
+              required: t('ticketFormDialog.errorTitleRequired'),
               minLength: {
                 value: 4,
-                message: t('ticketCreateDialog.errorTitleMinLength', { amount: 4 })
+                message: t('ticketFormDialog.errorTitleMinLength', { amount: 4 })
               }
             })}
             id="title"
@@ -130,14 +174,14 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
           />
         </div>
         <div className="flex flex-col space-y-1.5">
-          <Label htmlFor="description">{t('ticketCreateDialog.descriptionFormLabel')}</Label>
+          <Label htmlFor="description">{t('ticketFormDialog.descriptionFormLabel')}</Label>
 
           <Textarea
             {...register('description', {
-              required: t('ticketCreateDialog.errorDescriptionRequired'),
+              required: t('ticketFormDialog.errorDescriptionRequired'),
               minLength: {
                 value: 7,
-                message: t('ticketCreateDialog.errorDescriptionMinLength', { amount: 7 })
+                message: t('ticketFormDialog.errorDescriptionMinLength', { amount: 7 })
               }
             })}
             id="description"
@@ -148,7 +192,7 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
           />
 
           <Typography variant="note" className="text-right text-muted-foreground">
-            {t('ticketCreateDialog.descriptionCharsAmount', { amount: descriptionCharsLeft })}
+            {t('ticketFormDialog.descriptionCharsAmount', { amount: descriptionCharsLeft })}
           </Typography>
         </div>
       </div>
@@ -158,7 +202,7 @@ export default function TicketCreateDialog({ open, closeDialog }: ITicketCreateD
   return (
     <DialogComponent
       open={open}
-      title={t('ticketCreateDialog.title')}
+      title={t(`ticketFormDialog.title.${mode.toLowerCase()}`)}
       cancelButtonText={t('common.cancel')}
       confirmButtonText={t('common.confirm')}
       confirmButtonHandler={handleSubmit(submitHandler)}
