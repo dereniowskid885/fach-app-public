e3369b75b19eea471b7a191985dbb4250be87e23
Daniel <dereniowskid885@gmail.com>
2026-03-30 16:14:54 +0200

    FE - added current evaluation display in Evaluation Dialog, EvaluationList table improvements, added evaluation responseTime display

--- frontend/web/src/components/ui/TicketSpecialistEvaluationDialog.tsx
+++ frontend/web/src/components/ui/TicketSpecialistEvaluationDialog.tsx
@@ -10,13 +10,15 @@ import {
   Ticket,
   usePatchTicketsByIdEditEvaluationMutation,
   Evaluation
-} from '@/api/accountApi';
-import { getFormattedPriceAmount } from '@/utils/shared';
+} from '@/services/api/generated/accountApi';
+import { getFormattedPriceAmount, getFormattedResponseTime } from '@/utils/shared';
 import { toast } from 'sonner';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import { useErrorHandler } from '@/hooks/useErrorHandler';
 import { EActionType, ETimePickerType } from '@/enums/ui';
 import { ESupportedCurrency } from '@shared/enums/currency';
+import { Calendar, ChartColumn, Clock, DollarSign } from 'lucide-react';
+import { getLocaleDateString } from '@/utils/date';
 
 export interface ITicketSpecialistEvaluationDialog {
   open: boolean;
@@ -34,6 +36,7 @@ export default function TicketSpecialistEvaluationDialog({
   closeDialog
 }: ITicketSpecialistEvaluationDialog) {
   const t = useTranslations();
+  const currentLocale = useLocale();
   const maxMinutes = 1440; // 1 day
 
   const [priceInCents, setPriceInCents] = useState<number>(
@@ -115,11 +118,79 @@ export default function TicketSpecialistEvaluationDialog({
     setErrorMessage('');
   }, [minutes, priceInCents]);
 
+  const currentEvaluation =
+    mode === EActionType.EDIT ? (
+      <div className="space-y-4 rounded-xl bg-background p-4">
+        <div className="flex gap-2">
+          <ChartColumn size={16} strokeWidth={2.5} className="text-muted-foreground" />
+
+          <Typography variant="note-wide">
+            {t('evaluationDialog.currentEvaluationTitle')}
+          </Typography>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex gap-12">
+            <div className="flex items-center gap-3">
+              <div className="mr-1 flex h-6 w-6 shrink-0 items-center justify-center rounded-md border border-blue-200 bg-blue-50">
+                <Clock size={12} className="text-blue-600" />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <Typography variant="note" className="text-nowrap font-bold">
+                  {t('evaluation.responseTime')}
+                </Typography>
+
+                <Typography variant="note" className="font-semibold text-muted-foreground">
+                  {getFormattedResponseTime(currentUserEvaluation?.minutes, t)}
+                </Typography>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <div className="mr-1 flex h-6 w-6 shrink-0 items-center justify-center rounded-md border border-blue-200 bg-blue-50">
+                <Calendar size={12} className="text-blue-600" />
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <Typography variant="note" className="text-nowrap font-bold">
+                  {t('evaluation.dateOfResponse')}
+                </Typography>
+
+                <Typography variant="note" className="font-semibold text-muted-foreground">
+                  {getLocaleDateString(currentUserEvaluation?.dateOfResponse, currentLocale)}
+                </Typography>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <div className="mr-1 flex h-6 w-6 shrink-0 items-center justify-center rounded-md border border-emerald-200 bg-emerald-50">
+              <DollarSign size={12} className="text-emerald-600" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Typography variant="note" className="text-nowrap font-bold">
+                {t('evaluationDialog.currentEvaluationPrice')}
+              </Typography>
+
+              <Typography variant="note" className="font-semibold text-muted-foreground">
+                {getFormattedPriceAmount(
+                  currentUserEvaluation?.price?.amountInCents,
+                  currentUserEvaluation?.price?.currency
+                )}
+              </Typography>
+            </div>
+          </div>
+        </div>
+      </div>
+    ) : null;
+
   const ticketEvaluationForm = (
     <form>
       <div className="flex flex-col justify-center space-y-8">
         <div className="flex w-full flex-col items-center space-y-4">
-          <Typography variant="small">{t('evaluation.dateOfResponse')}</Typography>
+          <Typography variant="small">{t('evaluation.responseTime')}</Typography>
 
           <div className="flex space-x-3">
             <div className="flex flex-col items-center space-y-1">
@@ -180,15 +251,16 @@ export default function TicketSpecialistEvaluationDialog({
             onValueChange={minutes => setMinutes(minutes[0])}
           />
         </div>
+
         <div className="flex w-full flex-col items-center space-y-4">
           <Typography variant="small">{t('evaluation.price')}</Typography>
 
           <PriceInput
             className="w-auto text-center"
-            defaultInputValue={getFormattedPriceAmount(priceInCents).toString()}
+            defaultInputValue={getFormattedPriceAmount(priceInCents)}
             setPrice={setPriceInCents}
             max={10000}
-            currency={ESupportedCurrency.PLN}
+            currency={currentUserEvaluation?.price?.currency ?? ESupportedCurrency.PLN}
           />
         </div>
       </div>
@@ -204,7 +276,13 @@ export default function TicketSpecialistEvaluationDialog({
       confirmButtonHandler={submitHandler}
       isLoadingConfirmButton={isLoading}
       cancelButtonHandler={closeDialog}
-      content={ticketEvaluationForm}
+      content={
+        <>
+          {currentEvaluation}
+
+          {ticketEvaluationForm}
+        </>
+      }
       errorMessage={errorMessage}
       confirmButtonDisabled={!!errorMessage}
     />
--- frontend/web/src/utils/shared.ts
+++ frontend/web/src/utils/shared.ts
@@ -1,14 +1,35 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { User } from '@/api/accountApi';
+import { Currency, User } from '@/services/api/generated/accountApi';
+import { ESupportedCurrency } from '@shared/enums/currency';
+import { TFunction } from '@/types/i18n';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getFormattedPriceAmount = (amountInCents = 0) =>
-  amountInCents ? (amountInCents / 100).toFixed(2) : 0;
+export const getFormattedPriceAmount = (
+  amountInCents = 0,
+  currency?: Currency | ESupportedCurrency
+) => (amountInCents / 100).toFixed(2) + (currency ? ` ${currency}` : '');
 
 export const getUserFullName = (user?: User, nameFallback = 'Unknown') => {
   return user?.name && user?.surname ? `${user.name} ${user.surname}` : nameFallback;
+};
+
+export const getFormattedResponseTime = (minutesValue = 0, t: TFunction) => {
+  // divide total minutes by 1440 (minutes in a day) to get the number of full days.
+  const days = Math.floor(minutesValue / 1440);
+  // first get the remaining minutes after full days using modulo, then divide by 60 to get full hours.
+  const hours = Math.floor((minutesValue % 1440) / 60);
+  // return the remaining minutes after full hours using modulo 60.
+  const minutes = minutesValue % 60;
+
+  const result = [];
+
+  if (days > 0) result.push(t('responseTime.days', { count: days }));
+  if (hours > 0) result.push(t('responseTime.hours', { count: hours }));
+  if (minutes > 0) result.push(t('responseTime.minutes', { count: minutes }));
+
+  return result.join(' ');
 };
