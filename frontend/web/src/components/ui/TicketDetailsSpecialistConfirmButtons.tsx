e913608abcc1ede6031ed7aa57044bcab2163453
Daniel <dereniowskid885@gmail.com>
2026-05-17 15:38:51 +0200

    FE - patchTicketsById enhanced endpoint + Ticket Specialist send for review functionality

--- frontend/web/src/components/ui/TicketDetailsSpecialistConfirmButtons.tsx
+++ frontend/web/src/components/ui/TicketDetailsSpecialistConfirmButtons.tsx
@@ -1,31 +1,24 @@
 'use client';
 
 import { Button } from '../shadcn/button';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import TicketSpecialistEvaluationDialog from './TicketSpecialistEvaluationDialog';
 import { ETicketStatus } from 'shared-types';
-import {
-  Evaluation,
-  Ticket,
-  usePatchTicketsByIdMutation
-} from '@/services/api/generated/accountApi';
+import { Evaluation, Ticket } from '@/services/api/generated/accountApi';
 import { useTranslations } from 'next-intl';
 import { EActionType } from '@/enums/ui';
 import { TStatusActionButton } from '@/types/ticket';
 import TicketDetailsDialog from './TicketDetailsDialog';
-import { toast } from 'sonner';
-import { useErrorHandler } from '@/hooks/useErrorHandler';
+import TicketSpecialistSendForReviewDialog from './TicketSpecialistSendForReviewDialog';
 
 export interface ITicketDetailsSpecialistConfirmButtons {
   ticket: Ticket;
   currentUserEvaluation?: Evaluation;
-  setDialogErrorMessage?: Dispatch<SetStateAction<string | undefined>>;
 }
 
 export const TicketDetailsSpecialistConfirmButtons = ({
   ticket,
-  currentUserEvaluation,
-  setDialogErrorMessage
+  currentUserEvaluation
 }: ITicketDetailsSpecialistConfirmButtons) => {
   const ticketStatus = ticket.status as ETicketStatus;
 
@@ -36,37 +29,12 @@ export const TicketDetailsSpecialistConfirmButtons = ({
   );
   const [priceEvaluationDialog, setPriceEvaluationDialog] = useState<boolean>(false);
   const [ticketDetailsDialog, setTicketDetailsDialog] = useState<boolean>(false);
-
-  const [triggerUpdateTicket, { isLoading: isLoadingTicketUpdate, error: errorTicketUpdate }] =
-    usePatchTicketsByIdMutation();
-
-  useErrorHandler(errorTicketUpdate, {
-    setInlineError: message => setDialogErrorMessage?.(message)
-  });
-
-  const sendTicketForSolutionReview = async () => {
-    if (!ticket._id) return;
-
-    const result = await triggerUpdateTicket({
-      id: ticket._id,
-      body: {
-        status: ETicketStatus.SOLUTION_REVIEW
-      }
-    });
-
-    const { error } = result;
-    if (error) return;
-
-    setDialogErrorMessage?.('');
-    toast.success(t('ticketDetailsDialog.toastTitle.sendForReview'));
-  };
+  const [ticketSendForReviewDialog, setTicketSendForReviewDialog] = useState<boolean>(false);
 
   const statusActionButton: TStatusActionButton = {
     [ETicketStatus.IN_PROGRESS]: {
       title: t('ticketSpecialistActionButtons.sendForReview'),
-      handler: sendTicketForSolutionReview,
-      isLoading: isLoadingTicketUpdate,
-      isDisabled: ticket.specialistCommentsCount === 0
+      handler: () => setTicketSendForReviewDialog(true)
     },
     [ETicketStatus.AWAITING_EVALUATION]: currentUserEvaluation
       ? {
@@ -114,6 +82,12 @@ export const TicketDetailsSpecialistConfirmButtons = ({
         ticket={ticket}
         closeDialog={() => setTicketDetailsDialog(false)}
         scrollToInput={true}
+      />
+
+      <TicketSpecialistSendForReviewDialog
+        open={ticketSendForReviewDialog}
+        ticket={ticket}
+        closeDialog={() => setTicketSendForReviewDialog(false)}
       />
     </>
   );
