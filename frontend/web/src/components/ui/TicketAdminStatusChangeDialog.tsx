06031a7ff5d87f9eec9fccc75114c520276a322f
Daniel <dereniowskid885@gmail.com>
2026-05-17 15:39:16 +0200

    FE - ticket details admin buttons added

---
+++ frontend/web/src/components/ui/TicketAdminStatusChangeDialog.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import DialogComponent from '../common/DialogComponent';
+import { Ticket } from '@/services/api/generated/accountApi';
+import { useErrorHandler } from '@/hooks/useErrorHandler';
+import { toast } from 'sonner';
+import { useTranslations } from 'next-intl';
+import { RadioGroup, RadioGroupItem } from '../shadcn/radio-group';
+import { allowedTicketTransitions, ETicketStatus } from 'shared-types';
+import { ticketStatusObj } from '@/constants/ticketStatus';
+import { Field, FieldContent, FieldDescription, FieldLabel, FieldTitle } from '../shadcn/field';
+import TicketStatusIcon from './TicketStatusIcon';
+import { enhancedAccountApi } from '@/services/api/enhanced/enhancedAccountApi';
+import { getTicketUpdateErrorDescription } from '@/helpers/ticket';
+
+export interface ITicketAdminStatusChangeDialog {
+  open: boolean;
+  closeDialog: () => void;
+  ticket: Ticket;
+}
+
+export default function TicketAdminStatusChangeDialog({
+  open,
+  closeDialog,
+  ticket
+}: ITicketAdminStatusChangeDialog) {
+  const ticketStatus = ticket.status as ETicketStatus;
+
+  const t = useTranslations();
+
+  const [chosenTicketStatus, setChosenTicketStatus] = useState<ETicketStatus | null>(null);
+
+  const [triggerUpdateTicket, { isLoading: isLoadingTicketUpdate, error: errorTicketUpdate }] =
+    enhancedAccountApi.endpoints.patchTicketsById.useMutation();
+
+  useErrorHandler(errorTicketUpdate, {
+    toastDescription: getTicketUpdateErrorDescription(ticket, chosenTicketStatus, t)
+  });
+
+  const updateTicketHandler = async () => {
+    if (!ticket._id || !chosenTicketStatus) return;
+
+    const result = await triggerUpdateTicket({
+      id: ticket._id,
+      body: {
+        status: chosenTicketStatus
+      }
+    });
+
+    const { error } = result;
+    if (error) return;
+
+    closeDialog();
+    toast.success(t('ticketAdminStatusChangeDialog.toastTitle'));
+  };
+
+  const content = (
+    <RadioGroup defaultValue={ticket.status}>
+      {allowedTicketTransitions[ticketStatus]?.admin?.map(status => {
+        return (
+          <FieldLabel
+            key={status}
+            htmlFor={status}
+            className={`cursor-pointer ${ticketStatusObj[status].className}`}
+            onClick={() => setChosenTicketStatus(status)}
+          >
+            <Field orientation="horizontal">
+              <FieldContent className="space-y-2">
+                <FieldTitle>
+                  <TicketStatusIcon status={status} showStatusText={true} />
+                </FieldTitle>
+
+                <FieldDescription>
+                  {t(`ticketAdminStatusChangeDialog.statusChangeDescription.${status}`)}
+                </FieldDescription>
+              </FieldContent>
+
+              <RadioGroupItem
+                value={status}
+                id={status}
+                className={ticketStatusObj[status].className}
+              />
+            </Field>
+          </FieldLabel>
+        );
+      })}
+    </RadioGroup>
+  );
+
+  return (
+    <DialogComponent
+      open={open}
+      content={content}
+      title={t('ticketAdminStatusChangeDialog.title')}
+      cancelButtonText={t('common.cancel')}
+      cancelButtonHandler={closeDialog}
+      confirmButtonText={t('common.confirm')}
+      confirmButtonHandler={updateTicketHandler}
+      isLoadingConfirmButton={isLoadingTicketUpdate}
+    />
+  );
+}
